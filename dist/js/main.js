11a06144be47419b1e2e0dad16ba3372acf79582
tania <tani.zhelezniak@gmail.com>
2018-10-29 15:31:06 +0200

    tabs

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,4 +4,24 @@ $(function () {
         nextArrow:'<button type="button" class="but slick-next"></button>',
         dots: true
     });
+
+    //tabs-block-filter
+    $('.tabs-item').first().addClass('active');
+    $('.tabs-group').on('click', '.tabs-item:not(.active)', function (e) {
+        let target = e.target;
+        let a = $('.tabs-item a');
+        $(this)
+            .addClass('active')
+            .siblings()
+            .removeClass('active')
+            .closest('.tabs-section')
+            .find('.tabs-content')
+            .addClass('hidden')
+            .eq($(this)
+                .index())
+            .removeClass('hidden');
+        if(target !== a){
+            e.preventDefault();
+        }
+    });
 });
