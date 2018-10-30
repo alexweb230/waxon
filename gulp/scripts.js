2b021d250e324c5ad6133a650b1e20a4b1f9946e
tania <tani.zhelezniak@gmail.com>
2018-10-30 17:08:42 +0200

    tabs

--- gulp/scripts.js
+++ gulp/scripts.js
@@ -8,6 +8,7 @@ gulp.task("scripts", function () {
     return gulp.src([
         'app/js/libs/jquery/jquery.js',
         'app/js/libs/slick-slider/slick.js',
+        'node_modules/video.js/dist/video.min.js',
 
     ])
         .pipe(concat('libs.min.js'))
