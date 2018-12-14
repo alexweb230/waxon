e9c6cbfbdda16684bdb779aa80608b21e1bdc126
tania <tani.zhelezniak@gmail.com>
2018-12-14 15:30:46 +0200

    footer

--- app/js/main.js
+++ app/js/main.js
@@ -44,64 +44,78 @@ $(function () {
         playPase = document.querySelector('.play-pase'),
         fullscreen = document.querySelector('.full-size'),
         videoBox = document.querySelector('.video-box'),
-        range = document.getElementById('range');
+        range = document.querySelectorAll('.range');
 
 
-        // range.value = 0;
-        // range.min = 0;
-        // range.max = video.duration;
-
+    // range.value = 0;
+    // range.min = 0;
+    // range.max = video.duration;
 
 
     videoBox.addEventListener('click', function (e) {
         let target = e.target;
-        if(target === video ||  target === this || target === playPase){
-            if(video.paused){
+        if (target === video || target === this || target === playPase) {
+            if (video.paused) {
                 video.play();
                 playPase.classList.add('active');
                 this.classList.remove('overlay');
             }
-            else if(video.play){
-                 playPase.classList.remove('active');
-                 video.pause();
+            else if (video.play) {
+                playPase.classList.remove('active');
+                video.pause();
             }
         }
-        if(target === fullscreen){
+        if (target === fullscreen) {
             /* View in fullscreen */
-            if(video.requestFullscreen){
+            if (video.requestFullscreen) {
                 video.requestFullscreen();
-            }else if(video.mozRequestFullScreen){
+            } else if (video.mozRequestFullScreen) {
                 video.mozRequestFullScreen();
-            }else if(video.webkitRequestFullscreen){
+            } else if (video.webkitRequestFullscreen) {
                 video.webkitRequestFullscreen();
-            }else if (video.msRequestFullscreen) {
+            } else if (video.msRequestFullscreen) {
                 video.msRequestFullscreen();
             }
         }
 
     });
-   
+
+
     video.addEventListener('timeupdate', function () {
         let filledPos = video.currentTime / video.duration;
         filled.style.width = filledPos * 100 + '%';
-        if(video.ended){
+        if (video.ended) {
             playPase.classList.remove('active');
             videoBox.classList.add('overlay');
         }
     });
 
+    let rangeDown = true;
+
+    range.forEach(range => range.addEventListener('change', rangHandle));
+     range.forEach(range => range.addEventListener('mousemove', rangHandle));
+
+
+    // range.forEach(range => range.addEventListener('mousedown', () => rangeDown = true));
+
+
+    function rangHandle() {
+
+         video[this.name] = this.value;
+
+    }
+
+
     let mousedown = false;
 
-
-
-    progressBar.addEventListener('click',  scrub);
+    progressBar.addEventListener('click', scrub);
     progressBar.addEventListener('mousemove', (e) => mousedown && scrub(e));
     progressBar.addEventListener('mousedown', () => mousedown = true);
     progressBar.addEventListener('mouseup', () => mousedown = false);
 
-    function scrub (e) {
-         const scrubTime = (e.offsetX / progressBar.offsetWidth) * video.duration;
-         video.currentTime = scrubTime;
+    function scrub(e) {
+        const scrubTime = (e.offsetX / progressBar.offsetWidth) * video.duration;
+        video.currentTime = scrubTime;
     }
 });
 
